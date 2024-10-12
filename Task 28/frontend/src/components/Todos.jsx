4efe7345c8897d52b4dba7b8f940eea46fb7a139
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-10-12 13:44:44 +0530

    Update Todos.jsx

--- Task 28/frontend/src/components/Todos.jsx
+++ Task 28/frontend/src/components/Todos.jsx
@@ -1,71 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export function Todos({ todos, setTodos }) {
-    const handleMarkAsCompleted = async (id) => {    
-        try {
-            const response = await fetch("https://todo-backend-hc8l.onrender.com/completed", {                
-                method: "PUT",
+export function CreateTodo(){
+
+    //react-query
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+     return <div>
+
+        <input style={{
+            padding: 10,
+            margin: 10,
+            width: 200
+        }} type="text" placeholder="title" onChange={function(e){
+            const value = e.target.value;
+            setTitle(value)
+        }} /> <br />
+
+        <input style={{
+            padding: 10,
+            margin: 10,
+            width: 200
+        }}  type="text" placeholder="description" onChange={function(e){
+            const value = e.target.value;
+            setDescription(value)
+        }} /> <br />
+
+        <button style={{
+            padding: 10,
+            margin: 10
+        }} onClick={ () => {
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
                 headers: {
                     "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ id }),
-            });
+                }
+            })
+                .then(async function(res) {
+                    const json = await res.json();
+                    alert("Todo added")
 
-            if (!response.ok) {
-                const errorResponse = await response.json();
-                throw new Error(`Failed to mark todo as completed: ${errorResponse.msg}`);
-            }            
-            const updatedTodos = todos.map(todo => 
-                todo._id === id ? { ...todo, completed: true } : todo
-            );
-            setTodos(updatedTodos); 
-        } catch (error) {
-            alert("Error marking todo as completed 2");
-        }
-    };
-
-
-    const handleDeleteTodo = async (id) => {
-        try {
-            const response = await fetch(`https://todo-backend-hc8l.onrender.com/delete/${id}`, {
-                method: "DELETE",
-            });
-
-            if (!response.ok) {
-                const errorResponse = await response.json();
-                throw new Error(`Failed to delete todo: ${errorResponse.msg}`);
-            }
-
-            // Remove the deleted todo from the local state
-            const remainingTodos = todos.filter(todo => todo._id !== id);
-            setTodos(remainingTodos);
-
-            alert("Todo deleted successfully!");
-        } catch (error) {
-            alert("Error deleting todo");
-        }
-    };
-
-
-
-
-    return (
-        <div>
-            {todos.map(todo => (
-                <div key={todo._id}>
-                    <h1>{todo.title}</h1>
-                    <h2>{todo.description}</h2>
-                    <button onClick={() => handleMarkAsCompleted(todo._id)}>
-                        {todo.completed ? "Completed" : "Mark as completed"}
-                    </button>
-
-                    <button onClick={() => handleDeleteTodo(todo._id)}>
-                        Delete
-                    </button>
-                </div>
-            ))}
-
-            
-        </div>
-    );
+                    
+                })
+        }} >Add todo</button>
+     </div>
 }
