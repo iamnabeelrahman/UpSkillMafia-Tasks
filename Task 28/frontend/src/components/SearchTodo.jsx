c63da1540da31417a0bfba41fcff20a22888d4d0
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-10-12 13:47:44 +0530

    Create SearchTodo.jsx

---
+++ Task 28/frontend/src/components/SearchTodo.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const SearchTodo = () => {
+  const [title, setTitle] = useState('');
+  const [response, setResponse] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const url = new URL('http://localhost:3000/search');
+    url.searchParams.append('title', title);
+
+    try {
+      const res = await fetch(url, { method: 'GET' });
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const jsonResponse = await res.json();
+      setResponse(jsonResponse);
+    } catch (error) {
+      console.error('Error:', error);
+      setResponse({ error: 'Error fetching data' });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Search Your Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Title:
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {response.results && response.results.length > 0 && (
+        <div>
+          <h2>Results:</h2>
+          {response.results.map(todo => (
+            <div key={todo._id}>
+              <h3>{todo.title}</h3>
+              <p>{todo.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      {response.error && <p>{response.error}</p>}
+    </div>
+  );
+};
+
+export default SearchTodo;
