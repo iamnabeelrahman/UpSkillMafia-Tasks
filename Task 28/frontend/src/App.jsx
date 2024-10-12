3c05930c8786377abb83607e5aca6ae9f06172a4
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-10-12 13:44:04 +0530

    Update App.jsx

--- Task 28/frontend/src/App.jsx
+++ Task 28/frontend/src/App.jsx
@@ -5,12 +5,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
+import SearchTodo from './components/SearchTodo'; // Adjust the path if necessary
+
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    fetch("https://todo-backend-hc8l.onrender.com/todos")
+    fetch("http://localhost:3000/todos")
         .then(async function(res) {
             const json = await res.json();
             setTodos(json.todos);
@@ -22,8 +24,12 @@ function App() {
   return (
     <div>
       <h1>UpSkillMafia Task - 28</h1>
+      
       <CreateTodo />
       <Todos todos={todos} setTodos={setTodos} />
+      <div>
+            <SearchTodo/>
+        </div>
 
     </div>
   );
