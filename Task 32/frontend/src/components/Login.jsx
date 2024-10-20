b4e9e4222db4e50779ae600f60ff4df3545e79bd
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-10-20 20:24:43 +0530

    Update Login.jsx

--- Task 32/frontend/src/components/Login.jsx
+++ Task 32/frontend/src/components/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://task32-backend.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
